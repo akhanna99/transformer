f64786d33e78ea2ec42fc3e34e7915b8044a6c16
akhanna99 <arzukhanna@gmail.com>
2023-07-10 14:08:41 +1000

    update to gpt parameters

--- gpt.py
+++ gpt.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 16 # how many independent sequences will we process in parallel?
-block_size = 32 # what is the maximum context length for predictions?
+batch_size = 8 # how many independent sequences will we process in parallel?
+block_size = 16 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 100
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 64
+n_embd = 32
 n_head = 2
 n_layer = 4
 dropout = 0.0
@@ -18,8 +18,8 @@ dropout = 0.0
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+# wget https://www.gutenberg.org/cache/epub/11/pg11.txt
+with open('AliceInWonderland.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
