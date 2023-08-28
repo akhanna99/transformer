0463158d1243a9beb148e5901259e976a0f33ad4
akhanna99 <138735205+akhanna99@users.noreply.github.com>
2023-08-28 10:28:16 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 def process_input(input_data):
     # Implement your Python script logic here to process the input_data
     result = f"Processed input: {input_data}"
-    return result
+    print(result)
